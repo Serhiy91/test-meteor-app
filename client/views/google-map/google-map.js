97a4f0dafa013937b5410cd7fd12081a6df8fda3
Serhiy91 <serhiyzi@mail.ru>
2015-11-15 13:46:57 +0200

    formed right query object

--- client/views/google-map/google-map.js
+++ client/views/google-map/google-map.js
@@ -20,14 +20,28 @@ Template.map.helpers({
 Template.map.events({
 	'submit .main-form': function(e) {
 		e.preventDefault();
-		var map = GoogleMaps.maps.map.instance;
-		var queryObject = {
-			query: e.target[0].value,
-			lat: map.center.lat(),
-			lng: map.center.lng(),
-			zoom: map.zoom
+		var R_EARTH = 6378;
+		var RADIANS = 57.2958;
+
+		var bounds = GoogleMaps.maps.map.instance.getBounds();
+		var centerCords = {
+			lat: bounds.getCenter().lat(),
+			lng: bounds.getCenter().lng()
+		};
+		var northEastRadCords = {
+			lat: bounds.getNorthEast().lat() / RADIANS,
+			lng: bounds.getNorthEast().lng() / RADIANS
 		};
 
-		console.log(queryObject);
+		var distance = R_EARTH * Math.acos(Math.sin(centerCords.lat / RADIANS) *
+				Math.sin(northEastRadCords.lat) + Math.cos(centerCords.lat / RADIANS) *
+				Math.cos(northEastRadCords.lat) * Math.cos(northEastRadCords.lng - centerCords.lng / RADIANS));
+
+		var queryObject = {
+			query: e.target[0].value,
+			lat: centerCords.lat,
+			lng: centerCords.lng,
+			radius: distance
+		};
 	}
 });
