441d37a2250715d486585533cdb562749b88dd66
Serhiy91 <serhiyzi@mail.ru>
2015-11-17 12:42:12 +0200

    added ability show marks into map

--- client/views/venues-list/venues-list.js
+++ client/views/venues-list/venues-list.js
@@ -1,4 +1,23 @@
 Venues = new Meteor.Collection(null);
+var markers = [];
+
+Venues.find().observe({
+	added: function(venue) {
+		var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(venue.location.lat, venue.location.lng),
+			map: GoogleMaps.maps.map.instance,
+			id: venue._id
+		});
+		markers.push(marker);
+	},
+	removed: function(mark) {
+		for (var i = 0; i < markers.length; i++) {
+			markers[i].setMap(null);
+		}
+		console.log(markers.length);
+		markers = [];
+	}
+});
 
 Template.venuesList.helpers({
 	venues: function() {
