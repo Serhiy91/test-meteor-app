d7e7b2cf490f3c8ab3089c27f31b82de4e7a7a08
Serhiy91 <serhiyzi@mail.ru>
2015-11-18 12:03:43 +0200

    added info window for markers

--- client/views/venues-list/venues-list.js
+++ client/views/venues-list/venues-list.js
@@ -1,13 +1,30 @@
 Venues = new Meteor.Collection(null);
 var markers = [];
+var openInfoWindow;
 
 //make reactive map markers
 Venues.find().observe({
 	added: function(venue) {
+		var map = GoogleMaps.maps.map.instance;
+		var infoText = '<strong>' + venue.name + '</strong><br />' + venue.location.city + ' ' + venue.location.address;
+
 		var marker = new google.maps.Marker({
 			position: new google.maps.LatLng(venue.location.lat, venue.location.lng),
-			map: GoogleMaps.maps.map.instance
+			map: map,
+			title: venue.name
 		});
+
+		//show window with venue info by clicking on the mark
+		marker.info = new google.maps.InfoWindow({
+			content: infoText
+		});
+		google.maps.event.addListener(marker, 'click', function() {
+			//show only one info window at one time
+			if (openInfoWindow) openInfoWindow.close(map);
+			marker.info.open(map, marker);
+			openInfoWindow = marker.info;
+		});
+
 		markers.push(marker);
 	},
 	removed: function() {
